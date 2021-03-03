c80942904ea44e5b0ca75ccbadffb9d198207b28
Prathap <lingasaiprathapreddy555@gmail.com>
2021-03-03 23:40:25 +0530

    special bricks and normal bricks and level1 completed

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallCollision : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float ballVelocity;
+    public Transform paddle;
+    public float offset;
+    bool start = false;
+    //float Count;
+    public float SuperBrickCount;
+    public int brickCount;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //Bricks br = FindObjectOfType<Bricks>();
+        //Count = br.brick.Length;
+        brickCount = GameObject.FindGameObjectsWithTag("Brick").Length;
+        SuperBrickCount = GameObject.FindGameObjectsWithTag("SuperBrick").Length;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Paddle")
+        {
+            
+            //print("Paddle collision");
+        }
+        else if (collision.gameObject.tag == "Boundary")
+        {
+            
+            //print("Boundary collision");
+        }
+        else if (collision.gameObject.tag == "SuperBrick")
+        {
+            //Destroy(collision.gameObject);
+            //Count = Count - 0.5f;
+            SuperBrickCount = SuperBrickCount - 0.5f;
+        }
+        if(collision.gameObject.tag == "Brick")
+        {
+            brickCount--;
+        }
+        //if (Count <= 0)
+        //{
+        //    print("Game over");
+        //    SceneManager.LoadScene(1);
+        //}
+        if(SuperBrickCount <= 0 && brickCount<=0)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(1);
+            //print("Ball destroyed");
+        }
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0) && start == false)
+        {
+            rb.velocity = new Vector2(0, ballVelocity);
+            start = true;
+           // print("Ball launched");
+        }
+        if (start == false)
+        {
+            transform.position = new Vector2(paddle.position.x, paddle.position.y + offset);
+        }
+    }
+}
---
+++ Assets/Bricks.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bricks : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    public int health = 2;
+    //public int count = 8;
+    //public GameObject[] brick;
+    //public int brickCount;
+    
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        //brick = new GameObject[GameObject.FindGameObjectsWithTag("Brick").Length];
+        //print(brick.Length);
+        //brickCount = GameObject.FindGameObjectsWithTag("Brick").Length;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ball")
+        {
+            health--;
+            //brickCount--;
+            sprite.color = new Color(1, 0.4f, 0, 1);
+
+        }
+       
+        if(health == 1)
+        {
+            
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+       //if(brickCount == 0)
+       // {
+       //     SceneManager.LoadScene(1);
+       // }
+    }
+    }
+
